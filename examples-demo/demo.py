ee7a66a2088c0ea377a23be703b03b171aee98e5
pangyao <17791599647@163.com>
2026-09-17 22:29:06 +0800

    feat：update demo.py，add 新技术点

--- examples-demo/demo.py
+++ examples-demo/demo.py
@@ -7,6 +7,10 @@ client = OpenAI(
     base_url="https://open.bigmodel.cn/api/paas/v4/",
 )
 # 这是最新的写法，目前国内各大平台还赞时未支持
+# client.responses.create 接口。这是 OpenAI 最近新推出的一个非常新的 API 接口
+# （对应的底层路由是 /v1/responses）。但国内的大部分模型厂商（如智谱 GLM）尚未兼容这个新接口。
+
+# 目前各大厂商 100% 兼容的统一标准对话接口是 client.chat.completions.create
 # response = client.responses.create(
 #     model="gpt-5.5",
 #     instructions="You are a coding assistant that talks like a pirate.",
